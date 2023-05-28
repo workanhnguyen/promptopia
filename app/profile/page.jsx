a6abc3c9b6851ff5d720ec3a2e3aab14feae7b35
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-05-28 15:56:24 +0700

    update

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,6 +8,8 @@ import Profile from '@components/Profile';
 
 const MyProfile = () => {
   const [posts, setPosts] = useState([]);
+
+  const router = useRouter();
   const { data: session } = useSession();
 
   useEffect(() => {
@@ -21,8 +23,25 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(`Are you sure you want to delete this prompt?`);
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p) => p.id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,10 @@ import { usePathname, useRouter } from 'next/navigation';
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('');
 
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt); // copy text to clipboard
@@ -29,9 +33,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.email}</h3>
           </div>
         </div>
-        <div className='copy_btn' onClick={() => {handleCopy}}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={copied === post.prompt ? './assets/icons/tick.svg' : './assets/icons/copy.svg'}
+            alt='copy-btn'
             width={12}
             height={12} 
           />
@@ -44,6 +49,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p 
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
